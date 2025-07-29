92d583491c87da093ffbace34fb1fb0c3b8d18dc
lgachig <163140225+lgachig@users.noreply.github.com>
2025-07-28 20:42:51 -0500

    push con todo el contenido, para establecer la base

---
+++ src/main/java/uce/edu/ec/iniciospringboot/InicioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package uce.edu.ec.iniciospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicioSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InicioSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uce/edu/ec/iniciospringboot/InicioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package uce.edu.ec.iniciospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicioSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
