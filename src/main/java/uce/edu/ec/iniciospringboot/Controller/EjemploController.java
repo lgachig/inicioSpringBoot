6edd89c1465486f983cbd7fec27914f38b8ba2fc
lgachig <163140225+lgachig@users.noreply.github.com>
2025-07-29 16:34:21 -0500

    Refactor `Detalles_information` template and `EjemploController` to display a dynamic list of employees with Thymeleaf integration. @ModelAttribute

--- src/main/java/uce/edu/ec/iniciospringboot/Controller/EjemploController.java
+++ src/main/java/uce/edu/ec/iniciospringboot/Controller/EjemploController.java
@@ -3,7 +3,12 @@ package uce.edu.ec.iniciospringboot.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import uce.edu.ec.iniciospringboot.Models.Empleados;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 // Devuelve una vista(html)
 @Controller
@@ -11,11 +16,18 @@ public class EjemploController {
 
     // info devuelve la vista detalle_information
     @GetMapping("/")
-    public String info(Model model){
-        Empleados empleado = new Empleados("Juan", "Perez", "Elluis@uce.edu.ec", "999999999", 25);
-        model.addAttribute("Gerencia", empleado);
-
+    public String info(){
         return "Detalles_information";
     }
 
+    @ModelAttribute("Empleados")
+    public List<Empleados> listEmpleados(){
+        return Arrays.asList(
+                new Empleados("Juan", "Perez",null, "999999999", 25),
+                new Empleados("Juan", "Perez",null, "999999999", 25),
+                new Empleados("Juan", "Perez",null, "999999999", 25),
+                new Empleados("Juan", "Perez",null, "999999999", 25),
+                new Empleados("Juan", "Perez",null, "999999999", 25)
+        );
+    }
 }
